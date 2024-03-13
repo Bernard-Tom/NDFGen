7ce36f0f4b4f2da4503f1658ee11e8b0af225cc6
Tom <tomb.pers@gmail.com>
2024-03-13 15:21:35 +0100

    add config for csv file build

---
+++ data_config.py
@@ -0,0 +1,4 @@
+data_files = {'user_data.csv':'user_name;user_adress;code;user_bank',
+              'adress.csv':'name;street;postal;city',
+              'historic.csv':'date;start_name;start_street;start_postal;start_city;end_name;end_street;end_postal;end_city;distance;price;rtrn_state',
+              'travel':'start_name;end_name;distance'}
