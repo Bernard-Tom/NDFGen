a2fd180957005998c83279cda1caa9f6fbc653c3
Tom <tomb.pers@gmail.com>
2024-03-13 11:30:52 +0100

    final

--- source/main.py
+++ source/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from PyQt5.QtWidgets import (
     QWidget,QMainWindow,QApplication, QHBoxLayout,QStackedWidget
     )
@@ -9,28 +10,36 @@ class MainWindow(QMainWindow):
     """Main window of the application"""
     def __init__(self) -> None:
         super().__init__()
-        self.setWindowTitle('NDFGen')
-        self.setFixedWidth(800)
-        self.setFixedHeight(800)
-        self.UIComponents()
+        self.__title = 'NDF Gen'
+        self.__width = 800
+        self.__height = 800
+        self.ui_components()
 
-    def UIComponents(self)-> None:
+    def ui_components(self)-> None:
         """Set graphical components"""
+        self.setWindowTitle(self.__title)
+        self.setFixedWidth(self.__width)
+        self.setFixedHeight(self.__height)
+
         layout = QHBoxLayout()
         main_widget = QWidget(self)
         main_widget.setLayout(layout)
         self.setCentralWidget(main_widget)
 
         stackWidget = QStackedWidget()
-        self.historic_win = HistoricWin()
-        stackWidget.addWidget(self.historic_win)
+        historic_win = HistoricWin()
+        stackWidget.addWidget(historic_win)
         layout.addWidget(stackWidget)
 
-def main():
+def main() -> None:
     app = QApplication(sys.argv)
     w = MainWindow()
     w.show()
     app.exec()
 
+def setup() -> bool:
+    cwd = os.getcwd()
+    print()
+
 if __name__ == '__main__':
-    main()
+    setup()
