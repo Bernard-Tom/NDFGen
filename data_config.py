09c66e59567eaab84ede45be876639c08d8ee0b5
Tom <tomb.pers@gmail.com>
2024-03-14 07:28:52 +0100

    add data_config

--- data_config.py
+++
@@ -1,4 +0,0 @@
-data_files = {'user_data.csv':'user_name;user_adress;code;user_bank',
-              'adress.csv':'name;street;postal;city',
-              'historic.csv':'date;start_name;start_street;start_postal;start_city;end_name;end_street;end_postal;end_city;distance;price;rtrn_state',
-              'travel':'start_name;end_name;distance'}
